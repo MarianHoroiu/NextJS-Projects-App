f668ab41d151d929a7a15c7ce67ccf698f376c9f
Marian Horoiu <marian.horoiu@20robots.tech>
2023-06-20 08:32:12 +0300

    password management

---
+++ lib/auth.ts
@@ -0,0 +1,9 @@
+import bcrypt from "bcrypt";
+
+export const hashPassword = (password: string): Promise<string> =>
+  bcrypt.hash(password, 10);
+
+export const comparePasswords = (
+  plainTextPassword: string,
+  hashedPassword: string
+) => bcrypt.compare(plainTextPassword, hashedPassword);
