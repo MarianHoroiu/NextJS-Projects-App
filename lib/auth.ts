11f73e42c52f37ffeb2e9f391a37f1f4c11e4c83
Marian Horoiu <marian.horoiu@20robots.tech>
2023-06-21 08:49:29 +0300

    create JWT functions and set .env variables

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import { SignJWT, jwtVerify } from "jose";
+import { db } from "./db";
 
 export const hashPassword = (password: string): Promise<string> =>
   bcrypt.hash(password, 10);
@@ -7,3 +9,41 @@ export const comparePasswords = (
   plainTextPassword: string,
   hashedPassword: string
 ) => bcrypt.compare(plainTextPassword, hashedPassword);
+
+// JWT
+// FE-Master tutorial: https://frontendmasters.com/courses/fullstack-app-next-v2/json-web-tokens/
+export const createJWT = user => {
+  // return jwt.sign({ id: user.id }, 'cookies')
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60 * 24 * 7;
+
+  return new SignJWT({ payload: { id: user.id, email: user.email } })
+    .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+    .setExpirationTime(exp)
+    .setIssuedAt(iat)
+    .setNotBefore(iat)
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET)); // need to set JWT_SECRET in .env file
+};
+
+export const validateJWT = async jwt => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+
+  return payload.payload as any;
+};
+
+export const getUserFromCookie = async cookies => {
+  const jwt = cookies.get(process.env.COOKIE_NAME); // need to set COOKIE_NAME in .env file
+
+  const { id } = await validateJWT(jwt.value);
+
+  const user = await db.user.findUnique({
+    where: {
+      id: id as string,
+    },
+  });
+
+  return user;
+};
