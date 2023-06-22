0940fa87d6366f01c742554d9673a503717009eb
Marian Horoiu <marian.horoiu@20robots.tech>
2023-06-22 07:51:11 +0300

    create Greetings component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,20 @@
 import "@/styles/global.css";
 import GlassPane from "@/components/GlassPane";
+import Sidebar from "../../components/Sidebar";
+import { ReactElement } from "react";
 
-export default function DashboardRootLayout({ children }) {
+export default function DashboardRootLayout({
+  children,
+}: {
+  children: ReactElement;
+}) {
   return (
     <html lang="en">
       <head />
       <body className="h-screen w-screen candy-mesh p-6">
-        <GlassPane className="w-full h-full flex items-center justify-center">
+        <GlassPane className="w-full h-full flex items-center">
+          <Sidebar />
+
           {children}
         </GlassPane>
       </body>
